f4582bca4b86a98a008027b7460bdf951c11e069
Oscar Otero <oom@oscarotero.com>
2013-06-13 18:47:51 +0200

    Created autoloader file for non Composer instalations

--- test/img.php
+++ test/img.php
@@ -1,26 +1,7 @@
 <?php
 use Imagecow\Image;
 
-//A simple PSR-0 autoload function
-function autoload ($className) {
-	$className = ltrim($className, '\\');
-	$fileName = dirname(__DIR__).'/';
-	$namespace = '';
-	
-	if ($lastNsPos = strripos($className, '\\')) {
-		$namespace = substr($className, 0, $lastNsPos);
-		$className = substr($className, $lastNsPos + 1);
-		$fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	}
-
-	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-	if (is_file($fileName)) {
-		require $fileName;
-	}
-}
-
-spl_autoload_register('autoload');
+include('../Imagecow/autoloader.php');
 
 $transform = Image::getResponsiveOperations($_COOKIE['Imagecow_detection'], $_GET['transform']);
 
--- test/resize.php
+++ test/resize.php
@@ -4,26 +4,7 @@ ini_set('memory_limit', -1);
 
 use Imagecow\Image;
 
-//A simple PSR-0 autoload function
-function autoload ($className) {
-	$className = ltrim($className, '\\');
-	$fileName = dirname(__DIR__).'/';
-	$namespace = '';
-	
-	if ($lastNsPos = strripos($className, '\\')) {
-		$namespace = substr($className, 0, $lastNsPos);
-		$className = substr($className, $lastNsPos + 1);
-		$fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	}
-
-	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-	if (is_file($fileName)) {
-		require $fileName;
-	}
-}
-
-spl_autoload_register('autoload');
+include('../Imagecow/autoloader.php');
 
 foreach (glob(__DIR__.'/pictures/*') as $picture) {
 	if (!preg_match('/\/[0-9]+\.[a-z]{2,4}$/', $picture)) {

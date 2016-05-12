d0f4d7ec738bd37c0d8b7e03a8bc4cd9122bb225
oscarotero <oom@oscarotero.com>
2016-05-12 12:44:00 +0200

    implemented double value for positioning

--- src/Utils/Dimmensions.php
+++ src/Utils/Dimmensions.php
@@ -130,44 +130,17 @@ class Dimmensions
      */
     public static function getPositionValue($position, $newValue, $oldValue)
     {
+        $split = explode(' ', $position, 2);
+        $position = $split[0];
+        $offset = isset($split[1]) ? (int) $split[1] : 0;
+
         if (is_numeric($position)) {
-            return intval($position);
+            return intval($position) + $offset;
         }
 
         $newCenter = static::getIntegerValue($position, $newValue, true);
         $oldCenter = static::getIntegerValue($position, $oldValue, true);
 
-        return $oldCenter - $newCenter;
-    }
-
-    /**
-     * Calculates the x/y position.
-     *
-     * @param string|int|null $position
-     * @param int             $original
-     * @param int             $reference
-     * @param int             $padding
-     *
-     * @return int
-     */
-    public static function getPositionFromReferenceValue($position, $original, $reference, $padding = 0)
-    {
-        if (is_numeric($position)) {
-            $value = intval($position);
-        } else {
-            $value = static::getIntegerValue($position, $original, true);
-        }
-
-        $padding = static::getIntegerValue($padding, $original, true);
-
-        if (($position === 'top') || ($position === 'left')) {
-            $value = $padding;
-        } elseif (($position === 'bottom') || ($position === 'right')) {
-            $value -= $reference + $padding;
-        } elseif (($position === 'middle') || ($position === 'center')) {
-            $value = ($original / 2) - ($reference / 2);
-        }
-
-        return $value;
+        return $oldCenter - $newCenter + $offset;
     }
 }

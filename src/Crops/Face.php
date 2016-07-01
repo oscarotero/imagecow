efbebde8152082305c58756665fc0c2bd7b91469
Lito <lito@eordes.com>
2016-07-01 10:29:37 +0200

    Minor code style improvements

--- src/Crops/Face.php
+++ src/Crops/Face.php
@@ -29,12 +29,12 @@ class Face extends Entropy
      * @var array
      * @access protected
      */
-    protected static $safeZoneList;
+    protected static $safeZoneList = array();
 
     /**
      * originalImage
      *
-     * @var array
+     * @var object
      * @access protected
      */
     protected static $originalImage;
@@ -45,7 +45,7 @@ class Face extends Entropy
      * @var array
      * @access protected
      */
-    protected static $baseDimension;
+    protected static $baseDimension = array();
 
     /**
      * Returns the x,y values.
@@ -63,6 +63,7 @@ class Face extends Entropy
             'width' => $original->getImageWidth(),
             'height' => $original->getImageWidth(),
         ];
+
         parent::getOffsets($original, $targetWidth, $targetHeight);
     }
 
@@ -70,107 +71,116 @@ class Face extends Entropy
      * getBaseDimension
      *
      * @param string $key width|height
+     *
      * @access protected
+     *
      * @return int
      */
     protected static function getBaseDimension($key)
     {
         if (isset(static::$baseDimension)) {
             return static::$baseDimension[$key];
-        } elseif ($key == 'width') {
+        } elseif ($key === 'width') {
             return static::$originalImage->getImageWidth();
-        } else {
-            return static::$originalImage->getImageHeight();
         }
+
+        return static::$originalImage->getImageHeight();
     }
 
     /**
      * getFaceList get faces positions and sizes
      *
      * @access protected
+     *
      * @return array
      */
     protected static function getFaceList()
     {
         if (!function_exists('face_detect')) {
-            $msg = 'PHP Facedetect extension must be installed.
-                    See http://www.xarg.org/project/php-facedetect/ for more details';
-            throw new ImageException($msg);
+            throw new ImageException('PHP Facedetect extension must be installed. See http://www.xarg.org/project/php-facedetect/ for more details');
         }
 
         $faceList = static::getFaceListFromClassifier(self::CLASSIFIER_FACE);
+
         if (!is_array($faceList)) {
             $faceList = [];
         }
 
         $profileList = static::getFaceListFromClassifier(self::CLASSIFIER_PROFILE);
+
         if (!is_array($profileList)) {
             $profileList = [];
         }
 
-        $faceList = array_merge($faceList, $profileList);
-
-        return $faceList;
+        return array_merge($faceList, $profileList);
     }
 
     /**
      * getFaceListFromClassifier
      *
      * @param string $classifier
+     *
      * @access protected
+     *
      * @return array
      */
     protected static function getFaceListFromClassifier($classifier)
     {
         $file = tmpfile();
+
         static::$originalImage->writeImageFile($file);
-        $meta_data = stream_get_meta_data($file);
-        $faceList = face_detect($meta_data['uri'], __DIR__ . $classifier);
+
+        $faceList = face_detect(stream_get_meta_data($file)['uri'], __DIR__.$classifier);
+
         fclose($file);
+
         return $faceList;
     }
 
     /**
      * getSafeZoneList
      *
-     * @access private
+     * @access protected
+     *
      * @return array
      */
     protected static function getSafeZoneList()
     {
-        if (!isset(static::$safeZoneList)) {
-            static::$safeZoneList = array();
-        }
         // the local key is the current image width-height
-        $key = static::$originalImage->getImageWidth() . '-' . static::$originalImage->getImageHeight();
+        $key = static::$originalImage->getImageWidth().'-'.static::$originalImage->getImageHeight();
 
-        if (!isset(static::$safeZoneList[$key])) {
-            $faceList = static::getFaceList();
-
-            // getFaceList works on the main image, so we use a ratio between main/current image
-            $xRatio = static::getBaseDimension('width') / static::$originalImage->getImageWidth();
-            $yRatio = static::getBaseDimension('height') / static::$originalImage->getImageHeight();
-
-            $safeZoneList = array();
-            foreach ($faceList as $face) {
-                $hw = ceil($face['w'] / 2);
-                $hh = ceil($face['h'] / 2);
-                $safeZone = array(
-                    'left' => $face['x'] - $hw,
-                    'right' => $face['x'] + $face['w'] + $hw,
-                    'top' => $face['y'] - $hh,
-                    'bottom' => $face['y'] + $face['h'] + $hh
-                );
-
-                $safeZoneList[] = array(
-                    'left' => round($safeZone['left'] / $xRatio),
-                    'right' => round($safeZone['right'] / $xRatio),
-                    'top' => round($safeZone['top'] / $yRatio),
-                    'bottom' => round($safeZone['bottom'] / $yRatio),
-                );
-            }
-            static::$safeZoneList[$key] = $safeZoneList;
+        if (isset(static::$safeZoneList[$key])) {
+            return static::$safeZoneList[$key];
         }
+
+        $faceList = static::getFaceList();
+
+        // getFaceList works on the main image, so we use a ratio between main/current image
+        $xRatio = static::getBaseDimension('width') / static::$originalImage->getImageWidth();
+        $yRatio = static::getBaseDimension('height') / static::$originalImage->getImageHeight();
+
+        $safeZoneList = array();
+
+        foreach ($faceList as $face) {
+            $hw = ceil($face['w'] / 2);
+            $hh = ceil($face['h'] / 2);
+
+            $safeZone = array(
+                'left' => $face['x'] - $hw,
+                'right' => $face['x'] + $face['w'] + $hw,
+                'top' => $face['y'] - $hh,
+                'bottom' => $face['y'] + $face['h'] + $hh
+            );
+
+            $safeZoneList[] = array(
+                'left' => round($safeZone['left'] / $xRatio),
+                'right' => round($safeZone['right'] / $xRatio),
+                'top' => round($safeZone['top'] / $yRatio),
+                'bottom' => round($safeZone['bottom'] / $yRatio),
+            );
+        }
+
+        static::$safeZoneList[$key] = $safeZoneList;
 
         return static::$safeZoneList[$key];
     }
